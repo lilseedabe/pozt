648d9f2d1020b179a129cce468cacee38be43037
lilseedabe <114091647+lilseedabe@users.noreply.github.com>
2025-05-27 23:02:01 +0900

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // APIのベースURL
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL || '/api';
 
 // AxiosのデフォルトURLを設定
 const api = axios.create({
