ef82f09daa0107731ef363d8d8301f94059d3086
lilseedabe <114091647+lilseedabe@users.noreply.github.com>
2025-05-28 10:58:36 +0900

    Update index.jsx

--- frontend/src/components/RegionSelector/index.jsx
+++ frontend/src/components/RegionSelector/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useAppContext } from '../../context/AppContext';
 import { useCanvas } from '../../hooks/useCanvas';
 import { FiArrowUp, FiArrowDown, FiArrowLeft, FiArrowRight } from 'react-icons/fi';
